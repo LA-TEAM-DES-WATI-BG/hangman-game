2da7356411390359f99e9fd500bcc90ed417ac55
Ugo Ameslant <ugo.ameslant@ynov.com>
2021-11-15 14:29:29 +0100

    espace

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-func main(){
+func main() {
 	// var mot string
 	// func choix mot()
 	// for i:=0;i<len(mot);i++{
 	// 	func demande()
 	// func affiche()
 	// 	func erreur()
-	// 	func reussite()
+	// 	func  reussite()
 	// }
 
 }
