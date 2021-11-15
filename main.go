2381182945210f8a5f68f9c1920a31bbbaf56286
Ugo Ameslant <ugo.ameslant@ynov.com>
2021-11-15 14:28:18 +0100

    main

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main(){
+	// var mot string
+	// func choix mot()
+	// for i:=0;i<len(mot);i++{
+	// 	func demande()
+	// func affiche()
+	// 	func erreur()
+	// 	func reussite()
+	// }
+
+}
