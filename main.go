8786cfaefd604316ce80fdf8ecb616affab29337
Ugo Ameslant <ugo.ameslant@ynov.com>
2021-11-29 09:42:13 +0100

    final push

--- main.go
+++ main.go
@@ -3,21 +3,23 @@ package main
 import "fmt"
 
 func main() {
+	//var copie []int
 	var user_choice rune
 	var tab []int
 	var position []int
 	var wina bool
+	var tabletter []rune
 	bot_choice := rdmWord()
-	for nbError := 10; nbError > 0; {
+	for nbError := 10; nbError > -10; {
 		hangmanStatus(nbError)
 		fmt.Println()
-		user_choice = letterTrue()
+		user_choice = letterTrue(tabletter)
 		position = compare(user_choice, bot_choice)
+		tabletter = append(tabletter, user_choice)
 		for i := 0; i < len(position); i++ {
 			tab = append(tab, position[i])
 		}
 		if len(position) == 0 {
-
 			nbError--
 		}
 		affiche(tab, bot_choice)
@@ -26,7 +28,11 @@ func main() {
 			fmt.Println("vous avez win")
 			break
 		}
-		// 	func erreur()
-		// 	func  reussite()
+		fmt.Println(tab)
+		if nbError <= 0 {
+			fmt.Printf("Vous avez perdu le véritable mot était %s \n", bot_choice)
+			break
+		}
+
 	}
 }
